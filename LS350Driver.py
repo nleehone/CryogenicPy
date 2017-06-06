3b715a0769523eeeae1e161f0cbd2d96d2dc0ae4
Nicholas Lee-Hone <nicholas.leehone@gmail.com>
2017-06-06 07:51:00 -0700

    Update LS350 driver
    The original LS350 driver was based on the old design. Now it uses
    staticmethods and validation to get and check the commands.

--- LS350Driver.py
+++ LS350Driver.py
@@ -1,125 +1,215 @@
-import visa
-import sys
-import json
-from driver import DriverComponent
+import components as cmp
+import logging
+import time
+
+driver_queue = 'LS350.driver'
+controller_queue = 'LS350.controller'
+
+LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
+              '-35s %(lineno) -5d: %(message)s')
+LOGGER = logging.getLogger(__name__)
 
 
-class LS350Driver(DriverComponent):
-    def __init__(self, params):
-        super().__init__(params)
+class LS350Driver(cmp.DriverComponent):
+    @staticmethod
+    def validate_num_params(pars, num):
+        if len(pars) != num:
+            raise ValueError("Number of parameters ({}) does not match expectation ({})".format(len(pars), num))
 
-    @classmethod
-    def validate_channel(cls, channel):
-        if channel not in ['A', 'B', 'C', 'D']:
-            raise ValueError("Channel must be one of {'A', 'B', 'C', 'D'}")
+    @staticmethod
+    def validate_input_letter(input, include_all=True):
+        valid = ["A", "B", "C", "D"]
+        if include_all:
+            valid.append("0")
 
-    @classmethod
-    def validate_unit(cls, unit):
-        if unit not in ['K', 'C', 'S']:
-            raise ValueError("Units must be one of {'K', 'C', 'S'}.")
+        if input not in valid:
+            raise ValueError("Input must be one of {}, instead got {}".format(valid, input))
 
-    @classmethod
-    def set_temperature(cls, channel, temperature, unit='K'):
-        """"""
-        LS350Driver.validate_channel(channel)
-        LS350Driver.validate_unit(unit)
-        return 'SETP{}'.format(temperature)
+    @staticmethod
+    def validate_input_number(input):
+        if input not in [1, 2, 3, 4]:
+            raise ValueError("Input must be one of [1, 2, 3, 4], instead got {}".format(input))
 
-    @classmethod
-    def get_temperature_setpoint(cls, channel, unit='K'):
-        LS350Driver.validate_channel(channel)
-        LS350Driver.validate_unit(unit)
-        return 'SETP?{}'.format(channel)
+    @staticmethod
+    def validate_heater_output(output):
+        if output not in [1, 2]:
+            raise ValueError("Heater output must be one of [1, 2], instead got {}".format(output))
 
-    @classmethod
-    def get_temperature(cls, channel, unit='K'):
-        """"""
-        LS350Driver.validate_channel(channel)
-        LS350Driver.validate_unit(unit)
-        return '{}RDG?{}'.format(unit, channel)
+    @staticmethod
+    def get_identity(pars):
+        LS350Driver.get_identity_validate(pars)
+        return "*IDN?"
 
-    @classmethod
-    def get_sensor(cls, channel):
-        """"""
-        LS350Driver.validate_channel(channel)
-        return 'SRDG?{}'.format(channel)
+    @staticmethod
+    def get_identity_validate(pars):
+        LS350Driver.validate_num_params(pars, 0)
 
-    @classmethod
-    def set_P(cls, channel, P):
-        """"""
-        LS350Driver.validate_channel(channel)
-        return ""
+    @staticmethod
+    def reset(pars):
+        LS350Driver.reset_validate(pars)
+        return "*RST"
 
-    @classmethod
-    def get_P(cls, channel):
-        """"""
-        LS350Driver.validate_channel(channel)
+    @staticmethod
+    def reset_validate(pars):
+        LS350Driver.validate_num_params(pars, 0)
 
-    @classmethod
-    def set_I(cls, channel, I):
-        """
-        """
-        LS350Driver.validate_channel(channel)
+    @staticmethod
+    def get_brightness(pars):
+        LS350Driver.get_brightness_validate(pars)
+        return "BRIGT?"
 
-    @classmethod
-    def get_I(cls, channel):
-        """"""
-        LS350Driver.validate_channel(channel)
+    @staticmethod
+    def get_brightness_validate(pars):
+        LS350Driver.validate_num_params(pars, 0)
 
-    @classmethod
-    def set_D(cls, channel, D):
-        """"""
-        LS350Driver.validate_channel(channel)
+    @staticmethod
+    def set_brightness(pars):
+        LS350Driver.set_brightness_validate(pars)
+        return "BRIGT {}".format(*pars)
 
-    @classmethod
-    def get_D(cls, channel):
-        """"""
-        LS350Driver.validate_channel(channel)
+    @staticmethod
+    def get_temperature_celsius(pars):
+        LS350Driver.get_sensor_validate(pars)
+        return "CRDG? {}".format(*pars)
 
-    @classmethod
-    def set_PID(cls, channel, P, I=0, D=0):
-        """"""
-        LS350Driver.validate_channel(channel)
+    @staticmethod
+    def get_temperature_kelvin(pars):
+        LS350Driver.get_sensor_validate(pars)
+        return "KRDG? {}".format(*pars)
 
-    @classmethod
-    def get_PID(cls, channel):
-        """"""
-        LS350Driver.validate_channel(channel)
+    @staticmethod
+    def get_sensor_reading(pars):
+        LS350Driver.get_sensor_validate(pars)
+        return "SRDG? {}".format(*pars)
 
-    @classmethod
-    def set_heater_range(cls, channel, range):
-        """"""
-        LS350Driver.validate_channel(channel)
+    @staticmethod
+    def get_sensor_validate(pars):
+        LS350Driver.validate_num_params(pars, 1)
+        LS350Driver.validate_input(pars[0])
 
-    @classmethod
-    def get_heater_range(cls, channel):
-        """"""
-        LS350Driver.validate_channel(channel)
+    @staticmethod
+    def get_heater_output_percent(pars):
+        LS350Driver.get_heater_output_percent_validate(pars)
+        return "HTR? {output}".format(*pars)
 
-    @classmethod
-    def set_heater_power(cls, channel, power):
-        """"""
-        LS350Driver.validate_channel(channel)
+    @staticmethod
+    def get_heater_output_percent_validate(pars):
+        LS350Driver.validate_num_params(pars, 1)
+        LS350Driver.validate_heater_output(pars[0])
 
-    @classmethod
-    def get_heater_power(cls, channel):
-        """"""
-        LS350Driver.validate_channel(channel)
+    @staticmethod
+    def get_ramp_parameters(pars):
+        LS350Driver.get_ramp_parameters_validate(pars)
+        return "RAMP? {}".format(*pars)
 
-    @classmethod
-    def set_calibration_curve(cls, channel, curve):
-        """"""
-        LS350Driver.validate_channel(channel)
+    @staticmethod
+    def get_ramp_parameters_validate(pars):
+        LS350Driver.validate_num_params(pars, 1)
+        LS350Driver.validate_input_number(pars[0])
 
-    @classmethod
-    def get_calibration_curve(cls, channel):
-        """"""
-        LS350Driver.validate_channel(channel)
+    @staticmethod
+    def set_ramp_parameters(pars):
+        LS350Driver.set_ramp_parameters_validate(pars)
+        return "RAMP {},{},{}".format(*pars)
+
+    @staticmethod
+    def set_ramp_parameters_validate(pars):
+        LS350Driver.validate_num_params(pars, 3)
+        LS350Driver.validate_input_number(pars[0])
+        LS350Driver.validate_ramp_on_off(pars[1])
+        LS350Driver.validate_ramp_rate(pars[2])
+
+    @staticmethod
+    def validate_ramp_on_off(on_or_off):
+        if on_or_off not in [0, 1]:
+            raise ValueError("Ramp mode must be either 0=Off or 1=On, instead got {}".format(on_or_off))
+
+    @staticmethod
+    def validate_ramp_rate(rate):
+        if rate < 0 or rate > 100:
+            raise ValueError("Ramp rate must be between 0 and 100. 0 means infinite ramp rate.")
+
+    @staticmethod
+    def get_ramp_status(pars):
+        LS350Driver.get_ramp_status_validate(pars)
+        return "RAMPST? {}".format(*pars)
+
+    @staticmethod
+    def get_ramp_status_validate(pars):
+        LS350Driver.validate_num_params(pars, 1)
+        LS350Driver.validate_input_number(pars[0])
+
+    @staticmethod
+    def get_heater_range(pars):
+        LS350Driver.get_heater_range_validate(pars)
+        return "RANGE? {}".format(*pars)
+
+    @staticmethod
+    def get_heater_range_validate(pars):
+        LS350Driver.validate_num_params(pars, 1)
+        LS350Driver.validate_input_number(pars[0])
+
+    @staticmethod
+    def set_heater_range(pars):
+        LS350Driver.set_heater_range_validate(pars)
+        return "RANGE {},{}".format(*pars)
+
+    @staticmethod
+    def set_heater_range_validate(pars):
+        LS350Driver.validate_num_params(pars, 2)
+        LS350Driver.validate_input_number(pars[0])
+        LS350Driver.validate_heater_range(pars[0], pars[1])
+
+    @staticmethod
+    def validate_heater_range(output, heater_range):
+        if output in [1, 2]:
+            if heater_range not in range(0, 6):
+                raise ValueError("Heater range must be an integer between 0 and 5 for outputs [1, 2], instead got {}".format(heater_range))
+        elif output in [3, 4]:
+            if heater_range not in [0, 1]:
+                raise ValueError("Heater range must be either 0 or 1 for outputs [3, 4], instead got {}".format(heater_range))
+
+    @staticmethod
+    def get_reading_status(pars):
+        LS350Driver.get_reading_status_validate(pars)
+        return "RDGST? {}".format(pars)
+
+    @staticmethod
+    def get_reading_status_validate(pars):
+        LS350Driver.validate_num_params(pars, 1)
+        # We cannot get reading status for all inputs at the same time
+        LS350Driver.validate_input_letter(pars[0], include_all=False)
+
+    @staticmethod
+    def get_setpoint(pars):
+        LS350Driver.get_setpoint_validate(pars)
+        return "SETP? {}".format(*pars)
+
+    @staticmethod
+    def get_setpoint_validate(pars):
+        LS350Driver.validate_num_params(pars, 1)
+        LS350Driver.validate_input_number(pars[0])
+
+    @staticmethod
+    def set_setpoint(pars):
+        LS350Driver.set_setpoint_validate(pars)
+        return "SETP {},{}".format(*pars)
+
+    @staticmethod
+    def set_setpoint_validate(pars):
+        LS350Driver.validate_num_params(pars, 2)
+        LS350Driver.validate_input_number(pars[0])
 
 
 if __name__ == '__main__':
-    driver = LS350Driver(json.loads(sys.argv[1]))
-    driver.start()
-    while True:
+    logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+
+    driver = cmp.DriverComponent(driver_queue, {'library': 'instruments.yaml@mock',
+                                                'address': 'ASRL1::INSTR'})
+    try:
+        time.sleep(10)
+    except KeyboardInterrupt:
         pass
-    driver.shutdown()
+    finally:
+        pass
+        driver.close()
