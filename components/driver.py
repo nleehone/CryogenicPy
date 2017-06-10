9a917c1677b537ac971db927aa4e4afd59478797
Nicholas Lee-Hone <nicholas.leehone@gmail.com>
2017-06-09 17:26:32 -0700

    Implement command classes to simplify code

--- components/driver.py
+++ components/driver.py
@@ -1,4 +1,6 @@
 import visa
+import re
+from enum import Enum
 
 
 def validate_num_params(pars, num):
@@ -75,7 +77,55 @@ class Driver(object):
         return resp, None
 
 
+class CommandType(Enum):
+    GET = 0
+    SET = 1
+
+
+class Command(object):
+    cmd = ""
+    arguments = ""
+    num_args = 0
+    name = ""
+
+    @classmethod
+    def command(cls, pars):
+        cls.validate(pars)
+        return (cls.cmd + " " + cls.arguments.format(pars)).strip()
+
+    @classmethod
+    def validate(cls, pars):
+        validate_num_params(pars, cls.num_args)
+        cls._validate(pars)
+
+    @classmethod
+    def _validate(cls, pars):
+        pass
+
+
+class SetCommand(Command):
+    type = CommandType.SET
+
+
+class GetCommand(Command):
+    type = CommandType.GET
+
+    @classmethod
+    def result(cls, pars, result):
+        return result
+
+
+def find_subclasses(cls, type):
+    return {c.cmd: c for c in cls.__subclasses__() if issubclass(c, type)}
+
+
 class IEEE488_2_CommonCommands(Driver):
+    def __init__(self, params):
+        super().__init__(params)
+        self.get_commands = find_subclasses(self.__class__, GetCommand)
+        self.set_commands = find_subclasses(self.__class__, SetCommand)
+        self.all_commands = {**self.get_commands, **self.set_commands}
+
     def split_cmd(self, cmd):
         # Split the message into a command and a set of parameters
         command, *pars = list(filter(None, map(lambda x: x.strip(), re.split(',| |\?', cmd))))
@@ -87,6 +137,8 @@ class IEEE488_2_CommonCommands(Driver):
     def check_command(self, cmd):
         cmd, pars = self.split_cmd(cmd)
         try:
+            self.all_commands[cmd].validate(pars)
+            """try:
             {
                 "*CLS": IEEE488_2_CommonCommands.clear_status_validate,
                 "*ESE": IEEE488_2_CommonCommands.set_event_status_enable_validate,
@@ -101,21 +153,22 @@ class IEEE488_2_CommonCommands(Driver):
                 "*STB?": IEEE488_2_CommonCommands.get_status_byte_validate,
                 "*TST?": IEEE488_2_CommonCommands.self_test_validate,
                 "*WAI": IEEE488_2_CommonCommands.wait_validate,
-            }[cmd](pars)
+            }[cmd](pars)"""
         except Exception as e:
             return e
 
     def process_response(self, response, cmd):
         cmd, pars = self.split_cmd(cmd)
         try:
-            processed = {
+            processed = self.get_commands[cmd].result(pars, response)
+            """processed = {
                 "*ESE?": IEEE488_2_CommonCommands.get_event_status_enable_response,
                 "*ESR?": IEEE488_2_CommonCommands.get_event_status_register_response,
                 "*IDN?": IEEE488_2_CommonCommands.get_identification_response,
                 "*OPC?": IEEE488_2_CommonCommands.get_operation_complete_response,
                 "*SRE?": IEEE488_2_CommonCommands.get_service_request_enable_response,
                 "*STB?": IEEE488_2_CommonCommands.get_status_byte_response,
-            }[cmd](pars, response)
+            }[cmd](pars, response)"""
         except Exception as e:
             return None, e
         return processed, None
@@ -166,7 +219,10 @@ class IEEE488_2_CommonCommands(Driver):
     def get_event_status_register_response(pars, resp):
         return int(resp)
 
-    @staticmethod
+    class GetIdentification(GetCommand):
+        cmd = "*IDN?"
+
+    """@staticmethod
     def get_identification(pars):
         IEEE488_2_CommonCommands.get_identification_validate(pars)
         return "*IDN?"
@@ -177,7 +233,7 @@ class IEEE488_2_CommonCommands(Driver):
 
     @staticmethod
     def get_identification_response(pars, resp):
-        return resp
+        return resp"""
 
     @staticmethod
     def set_operation_complete(pars):
