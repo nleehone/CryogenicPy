e90c388e4768cf962fa4e30e5275dcbe996cf8c3
Nicholas Lee-Hone <nicholas.leehone@gmail.com>
2017-06-13 12:14:07 -0700

    Add regex matching to find float values

--- SMSPowerSupplyDriver.py
+++ SMSPowerSupplyDriver.py
@@ -22,6 +22,29 @@ class SMSPowerSupplyDriver(cmp.CommandDriver):
         if units not in ['T', 'A']:
             raise ValueError("Units must be either T or A, instead got {}".format(units))
 
+    @staticmethod
+    def strip_message_type(message):
+        """Separates the message into a message_type and the real message
+
+        Messages come from the SMS in the format '******** Message'
+        The 8 * characters represent the message_type and is always followed by a space.
+        """
+        # Remove any unwanted whitespace at start and end of message
+        message = message.strip()
+        message_head = message[:8]
+        message = message[9:]
+        if message_head == "........":
+            message_type = "status_confirmation"
+        elif message_head == "=======>":
+            message_type = "fault_report"
+        elif message_head == "------->":
+            message_type = "command_information"
+        elif message_head == "        ":  # 8 spaces
+            message_type = "controller identification"
+        else:  # Should have the format HH:MM:SS
+            message_type = "status_update"
+        return message_type, message
+
     class GetMid(QueryCommand):
         cmd = "MID?"
         arguments = "{}"
@@ -45,6 +68,10 @@ class SMSPowerSupplyDriver(cmp.CommandDriver):
 
         @classmethod
         def process_result(cls, driver, cmd, pars, result):
+            message_type, result = SMSPowerSupplyDriver.strip_message_type(result)
+            found = re.search(r'[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?', result)
+            if found:
+                print(found, result)
             print(result)
 
 
@@ -52,11 +79,11 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 
     driver = SMSPowerSupplyDriver(driver_queue, {'library': '',
-                                                'address': 'ASRL9::INSTR',
-                                                'baud_rate': 9600,
-                                                'parity': 'none',
-                                                'data_bits': 8,
-                                                'termination': 'x13'})
+                                                 'address': 'ASRL9::INSTR',
+                                                 'baud_rate': 9600,
+                                                 'parity': 'none',
+                                                 'data_bits': 8,
+                                                 'termination': 'x13'})
 
     try:
         time.sleep(1000000)
