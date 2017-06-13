5afdd30070ed37edd9da26b4f440127b4a011479
Nicholas Lee-Hone <nicholas.leehone@gmail.com>
2017-06-13 08:42:57 -0700

    Move DriverComponent into driver.py

--- components/driver.py
+++ components/driver.py
@@ -1,6 +1,72 @@
+from . import rmq_component as rmq
+from .components import Component
 import visa
 import re
+import traceback
 from enum import Enum
+
+
+class DriverComponent(rmq.RmqComponent, Component):
+    """Single point of communication with the instrument
+
+    Having a common point of communication prevents multiple parts of the system from
+    trying to access the hardware at the same time.
+
+    It is up to the user to make sure that only one instance of the Driver is ever running.
+    """
+    def __init__(self, driver_queue, driver_params, driver_class, **kwargs):
+        self.driver = driver_class(driver_params)
+        self.driver_queue = driver_queue
+        super().__init__(**kwargs)
+
+    def init_queues(self):
+        self.channel.queue_declare(queue=self.driver_queue)
+
+    def process(self):
+        method, properties, body = self.channel.basic_get(queue=self.driver_queue,
+                                                          no_ack=True)
+        if method is not None:
+            t0 = time.time()
+            result, error = self.process_command(body)
+            print("RESULT: ", result, error)
+            t1 = time.time()
+            reply = {"t0": t0,
+                     "t1": t1,
+                     "result": result,
+                     "error": ["".join(traceback.format_exception(etype=type(e),value=e,tb=e.__traceback__) if e else "") for e in error] if error is not None else ""}
+            print(body)
+            print(reply, result, error)
+            if result != [] or error != []:
+                print(properties.reply_to, reply)
+                self.channel.basic_publish('', routing_key=properties.reply_to, body=json.dumps(reply))
+
+    def process_command(self, body):
+        # METHOD: {READ, WRITE, QUERY}
+        body = json.loads(body.decode('utf-8'))
+        try:
+            method = body['METHOD']
+            if method not in ['WRITE', 'QUERY', 'READ']:
+                error = "Unrecognized METHOD: {}".format(method)
+                self.logger.warning(error)
+                return None, error
+
+            cmd = body['CMD']
+            results = []
+            errors = []
+            for command in cmd.split(';'):
+                if method == 'WRITE':
+                    self.driver.write(command)
+                elif method == 'QUERY':
+                    r, e = self.driver.query(command)
+                    results.append(r)
+                    errors.append(e if e is not None else "")
+                elif method == 'READ':
+                    r, e = self.driver.read()
+                    results.append(r)
+                    errors.append(e)
+            return results, errors
+        except AttributeError:
+            self.logger.warning("Invalid command format: {}".format(body))
 
 
 def validate_num_params(pars, num):
