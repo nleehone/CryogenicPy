4c3153bfd814db811e517eeb690dc6a49e2418bd
Nicholas Lee-Hone <nicholas.leehone@gmail.com>
2017-06-15 13:55:17 -0700

    LS218.driver
    Added ALARM?, ALARMST?, ANALOG?, BAUD? commands

--- LS218Driver.py
+++ LS218Driver.py
@@ -22,6 +22,14 @@ class LS218Driver(cmp.IEEE488_2_CommonCommands):
         except ValueError as e:
             raise ValueError("Input must be an integer between 0 and 9, instead got {}".format(input))
 
+    @staticmethod
+    def validate_output_number(input):
+        try:
+            if int(input) not in [1, 2]:
+                raise ValueError("Input must be one of {}, instead got {}".format([1, 2], input))
+        except ValueError as e:
+            raise ValueError("Input must be 1 or 2, instead got {}".format(input))
+
     class GetSensorReading(QueryCommand):
         cmd = "SRDG?"
         arguments = "{}"
@@ -44,6 +52,75 @@ class LS218Driver(cmp.IEEE488_2_CommonCommands):
     class GetCelsiusReading(GetSensorReading):
         cmd = "CRDG?"
 
+    class GetAlarmParameters(QueryCommand):
+        cmd = "ALARM?"
+        arguments = "{}"
+
+        @classmethod
+        def _validate(cls, pars):
+            LS218Driver.validate_input_number(pars[0])
+
+        @classmethod
+        def process_result(cls, driver, cmd, pars, result):
+            resp = list(map(lambda x: x.strip(), result.split(',')))
+            return {"on/off": int(resp[0]),
+                    "source": int(resp[1]),
+                    "high": float(resp[2]),
+                    "low": float(resp[3]),
+                    "deadband": float(resp[4]),
+                    "latch": int(resp[5])}
+
+    class GetAlarmStatus(QueryCommand):
+        cmd = "ALARMST?"
+        arguments = "{}"
+
+        @classmethod
+        def _validate(cls, pars):
+            LS218Driver.validate_input_number(pars[0])
+
+        @classmethod
+        def process_result(cls, driver, cmd, pars, result):
+            resp = list(map(lambda x: x.strip(), result.split(',')))
+            return {"high": int(resp[0]),
+                    "low": int(resp[1])}
+
+    class GetAnalogOutputParameters(QueryCommand):
+        cmd = "ANALOG?"
+        arguments = "{}"
+
+        @classmethod
+        def _validate(cls, pars):
+            LS218Driver.validate_output_number(pars[0])
+
+        @classmethod
+        def process_result(cls, driver, cmd, pars, result):
+            resp = list(map(lambda x: x.strip(), result.split(',')))
+            return {"bipolar": int(resp[0]),
+                    "mode": int(resp[1]),
+                    "input": int(resp[2]),
+                    "source": int(resp[3]),
+                    "high": float(resp[4]),
+                    "low": float(resp[5]),
+                    "manual": float(resp[6])}
+
+    class GetAnalogOutputData(QueryCommand):
+        cmd = "AOUT?"
+        arguments = "{}"
+
+        @classmethod
+        def _validate(cls, pars):
+            LS218Driver.validate_output_number(pars[0])
+
+        @classmethod
+        def process_result(cls, driver, cmd, pars, result):
+            return float(result)
+
+    class GetBaudRate(QueryCommand):
+        cmd = "BAUD?"
+
+        @classmethod
+        def process_result(cls, driver, cmd, pars, result):
+            return int(result)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
